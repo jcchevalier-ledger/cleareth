7621609e737899827d42c4f0044a6fae6be7e159
Jean-Christophe Chevalier <jean-christophe.chevalier@ledger.fr>
2023-09-17 12:06:40 +0200

    better formatting

--- codec/src/main/scala/cleareth/encoding/EvmEncoder.scala
+++ codec/src/main/scala/cleareth/encoding/EvmEncoder.scala
@@ -2,11 +2,8 @@ package cleareth.encoding
 
 import cats.Contravariant
 import cats.data.State
-import cats.syntax.contravariant.*
 import cats.syntax.monoid.*
-import cleareth.model.Address
 import scala.deriving.Mirror
-import scala.math.*
 import scodec.bits.ByteVector
 
 trait EvmEncoder[A] extends Serializable:
@@ -21,50 +18,6 @@ object EvmEncoder:
 
   final def apply[A](using instance: EvmEncoder[A]): EvmEncoder[A]        = instance
   final def apply[A](value: A)(using instance: EvmEncoder[A]): ByteVector = instance.encode(value)
-
-  given EvmEncoder[Int] = (value: Int) =>
-    State(offset => offset -> EncodingAcc.static(ByteVector.fromInt(value).padLeft(32)))
-  given EvmEncoder[Long] = (value: Long) =>
-    State(offset => offset -> EncodingAcc.static(ByteVector.fromLong(value).padLeft(32)))
-  given EvmEncoder[BigInt] = (value: BigInt) =>
-    State(offset => offset -> EncodingAcc.static(ByteVector(value.toByteArray).padLeft(32)))
-  given EvmEncoder[Address] = (value: Address) => State(offset => offset -> EncodingAcc.static(value.value.padLeft(32)))
-  given EvmEncoder[Boolean] = (bool: Boolean) =>
-    State { offset =>
-      offset -> EncodingAcc.static(ByteVector.fromValidHex(if (bool) "0x01" else "0x00").padLeft(32))
-    }
-
-  given EvmEncoder[String] = (value: String) =>
-    State { offset =>
-      val cont          = ByteVector(value.getBytes)
-      val paddedContent = cont.padRight((cont.length / 32 + 1) * 32)
-      val length        = ByteVector.fromLong(cont.length).padLeft(32)
-      val newOffset     = offset + length.length + paddedContent.length
-      newOffset -> EncodingAcc.dynamic(ByteVector.fromLong(offset).padLeft(32), length ++ paddedContent)
-    }
-
-  given [T](using EvmEncoder[T]): EvmEncoder[Seq[T]] = (value: Seq[T]) =>
-    State { offset =>
-      val cont = value
-        .map(EvmEncoder[T].stateEncode)
-        .foldLeft(State.empty[Long, EncodingAcc])((acc, next) =>
-          for
-            a <- acc
-            n <- next
-          yield a |+| n
-        )
-        .run(32 * value.length)
-        .value
-        ._2
-        .merge
-      val noOfElements = ByteVector.fromLong(value.length).padLeft(32)
-      val newOffset    = offset + noOfElements.length + cont.length
-      newOffset -> EncodingAcc.dynamic(ByteVector.fromLong(offset).padLeft(32), noOfElements ++ cont)
-    }
-
-  given [T](using EvmEncoder[T]): EvmEncoder[Array[T]] = EvmEncoder[Seq[T]].contramap(_.toSeq)
-  given [T](using EvmEncoder[T]): EvmEncoder[List[T]]  = EvmEncoder[Seq[T]].contramap(_.toSeq)
-  given [T](using EvmEncoder[T]): EvmEncoder[Set[T]]   = EvmEncoder[Seq[T]].contramap(_.toSeq)
 
   inline given derived[T](using m: Mirror.Of[T]): EvmEncoder[T] =
     new EvmEncoder[T]:
--- codec/src/main/scala/cleareth/encoding/package.scala
+++ codec/src/main/scala/cleareth/encoding/package.scala
@@ -1,8 +1,13 @@
 package cleareth
 
+import cats.data.State
+import cats.syntax.contravariant.*
+import cats.syntax.semigroup.*
+import cleareth.model.Address
 import scala.compiletime.erasedValue
 import scala.compiletime.summonFrom
 import scala.deriving.Mirror
+import scodec.bits.ByteVector
 
 package object encoding:
   inline final private[encoding] def summonEncoders[T <: Tuple]: List[EvmEncoder[?]] =
@@ -12,5 +17,51 @@ package object encoding:
 
   inline final private[encoding] def summonEncoder[A]: EvmEncoder[A] =
     summonFrom:
-      case encodeA: EvmEncoder[A] => encodeA
-      case _: Mirror.Of[A]        => EvmEncoder.derived[A]
+        case encodeA: EvmEncoder[A] => encodeA
+        case _: Mirror.Of[A]        => EvmEncoder.derived[A]
+
+  given EvmEncoder[Int] = (value: Int) =>
+    State(offset => offset -> EncodingAcc.static(ByteVector.fromInt(value).padLeft(32)))
+
+  given EvmEncoder[Long] = (value: Long) =>
+    State(offset => offset -> EncodingAcc.static(ByteVector.fromLong(value).padLeft(32)))
+
+  given EvmEncoder[BigInt] = (value: BigInt) =>
+    State(offset => offset -> EncodingAcc.static(ByteVector(value.toByteArray).padLeft(32)))
+
+  given EvmEncoder[Address] = (value: Address) => State(offset => offset -> EncodingAcc.static(value.value.padLeft(32)))
+
+  given EvmEncoder[Boolean] = (bool: Boolean) =>
+    State(offset => offset -> EncodingAcc.static(ByteVector.fromValidHex(if (bool) "0x01" else "0x00").padLeft(32)))
+
+  given EvmEncoder[String] = (value: String) =>
+    State { offset =>
+      val cont          = ByteVector(value.getBytes)
+      val paddedContent = cont.padRight((cont.length / 32 + 1) * 32)
+      val length        = ByteVector.fromLong(cont.length).padLeft(32)
+      val newOffset     = offset + length.length + paddedContent.length
+      newOffset -> EncodingAcc.dynamic(ByteVector.fromLong(offset).padLeft(32), length ++ paddedContent)
+    }
+
+  given [T](using EvmEncoder[T]): EvmEncoder[Seq[T]] = (value: Seq[T]) =>
+    State { offset =>
+      val cont = value
+        .map(EvmEncoder[T].stateEncode)
+        .foldLeft(State.empty[Long, EncodingAcc])((acc, next) =>
+          for
+            a <- acc
+            n <- next
+          yield a |+| n
+        )
+        .run(32 * value.length)
+        .value
+        ._2
+        .merge
+      val noOfElements = ByteVector.fromLong(value.length).padLeft(32)
+      val newOffset    = offset + noOfElements.length + cont.length
+      newOffset -> EncodingAcc.dynamic(ByteVector.fromLong(offset).padLeft(32), noOfElements ++ cont)
+    }
+
+  given [T](using EvmEncoder[T]): EvmEncoder[Array[T]] = EvmEncoder[Seq[T]].contramap(_.toSeq)
+  given [T](using EvmEncoder[T]): EvmEncoder[List[T]]  = EvmEncoder[Seq[T]].contramap(_.toSeq)
+  given [T](using EvmEncoder[T]): EvmEncoder[Set[T]]   = EvmEncoder[Seq[T]].contramap(_.toSeq)
